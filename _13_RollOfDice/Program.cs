9cd40924df04df1c2731602b509b6e66b30719f9
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-09 16:16:02 +0100

    adds dies object and arrays with statistics, roll dies the asked no of times, sum up dies and add to statistics

--- _13_RollOfDice/Program.cs
+++ _13_RollOfDice/Program.cs
@@ -6,6 +6,24 @@ namespace _13_RollOfDice
     {
         public static void Main(string[] args)
         {
+            var die1 = new Dies();
+            var die2 = new Dies();
+
+            int[] stats = new int[13];
+            int[,] rolls = new int[7, 7];
+
+            Console.WriteLine("Please enter how many times you want to roll the dice: ");
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var die1Value = die1.DieRoll();
+                var die2Value = die2.DieRoll();
+
+                var sum = die1Value + die2Value;
+                stats[sum] += 1;
+            }
+
         }
     }
 }
